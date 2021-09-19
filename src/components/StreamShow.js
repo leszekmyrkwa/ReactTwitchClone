dc80951c6be411f6179959fed0b7aebe9c723854
Leszek Myrkwa <myrkwa97@gmail.com>
2021-09-19 12:53:47 +0100

    smallchagne to stream show

--- src/components/StreamShow.js
+++ src/components/StreamShow.js
@@ -41,7 +41,7 @@ class StreamShow extends React.Component {
 
         return(
             <div>
-                <video ref={ this.video } style={{ width: '100%' }} controls/>
+                <video ref={this.videoRef} style={{ width: '100%' }} controls />
                 <h1>{ this.props.stream.title }</h1>
                 <h5>{ this.props.stream.description }</h5>
             </div>
